c50b754663dfbff5c672b4dd526e029c77656d4f
PedroMonteiro7 <21192977@CFP127ALUNO.LOCAL>
2021-08-27 11:30:04 -0300

    Segundo commit

---
+++ funcoes-alunos.php
@@ -0,0 +1,58 @@
+<link rel="stylesheet" href="style.css">
+
+<?php
+
+// Imprimir na tela a média das notas de todos os alunos
+function calcularMedia(array $turma) {
+    $soma = 0;
+    foreach($turma as $aluno) {
+        $soma = $soma + $aluno["nota"];
+    }
+
+    $media = $soma / count($turma);
+
+    return $media;
+}
+
+//criar uma função que apresente o aluno com a maior nota
+function alunoComMaiorNota(array $turma) {
+
+    $melhorAluno = null;
+    foreach($turma as $aluno) {
+        if($melhorAluno == null) {
+            $melhorAluno = $aluno;
+        } elseif($aluno["nota"] > $melhorAluno["nota"]) {
+            $melhorAluno = $aluno;
+        } elseif($aluno["nota"] == $melhorAluno["nota"]){}
+    }
+
+    return $melhorAluno;
+}
+
+//criar uma função que altere a nota de determinado aluno
+// & - passagem de parâmetro por referência
+function alterarNotaAluno(array &$turma, $nome, $novaNota) {
+    foreach($turma as $chave => $aluno) {
+        if($aluno["nome"] == $nome) {
+            $turma[$chave]["nota"] = $novaNota;
+        } 
+    }
+
+}
+
+alterarNotaAluno($alunos, "Isabela", 20);
+
+//escreva uma função que inclua a situação de todos os alunos
+//caso a nota for menor que 50 situação = reprovado
+//caso a nota for maior ou igual a 50, situação = aprovado
+function situacaoAlunos(array &$turma) {
+    foreach($turma as $chave => $aluno) {
+        if($aluno["nota"] >= 50) {
+            $turma[$chave]["situacao"] = '<span class="aprovado">Aprovado(a)</span>';
+        } else {
+            $turma[$chave]["situacao"] = '<span class="reprovado">Reprovado(a)</span>';
+        }
+    }
+}
+
+situacaoAlunos($alunos);
---
+++ index.php
@@ -0,0 +1,42 @@
+<?php
+
+    require("./alunos.php");
+    require("./funcoes-alunos.php");
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Notas dos Alunos</title>
+</head>
+<body>
+    <section>
+        <h1>Boletim</h1>
+        <table>
+            <tr>
+                <th>Nome</th>
+                <th>Idade</th>
+                <th>Nota</th>
+                <th>Situação</th>
+            </tr>
+            <?php
+                foreach($alunos as $aluno) {
+            ?>
+            <tr>
+                <td><?= $aluno["nome"] ?></td>
+                <td><?= $aluno["idade"] ?></td>
+                <td><?= $aluno["nota"] ?></td>
+                <td><?= $aluno["situacao"] ?></td>
+            </tr>
+            <?php
+            }
+            ?>
+        </table>
+    </section>
+</body>
+</html>
